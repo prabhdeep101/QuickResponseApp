19922ecbb9cba2b36b114cfd1ae3fd0e38a95250
Mel <melissa@foxit.co.nz>
2025-05-20 16:26:09 +1200

    Contacts

---
+++ app/src/main/java/com/example/quickresponseapp/contacts/ContactsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.quickresponseapp.contacts
+
+import androidx.fragment.app.Fragment
+import com.example.todolist.R
+
+class ContactsFragment : Fragment(R.layout.fragment_contacts) {
+}
