9b0c2183bf7b1c2099fba5f97d8365f973410fb7
JontySN <jonty.southcombe@gmail.com>
2025-05-20 12:53:47 +1200

    home screen buttons

---
+++ app/src/main/java/com/example/quickresponseapp/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.quickresponseapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.todolist.R
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen)
+    }
+}
