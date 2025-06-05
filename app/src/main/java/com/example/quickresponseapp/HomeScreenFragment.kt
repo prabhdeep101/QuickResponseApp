9b784de0cbe3ecf4e1b99748e9440c7c23b4ac9b
Mel <melissa@foxit.co.nz>
2025-06-05 13:28:13 +1200

    Nav_Graph

--- app/src/main/java/com/example/quickresponseapp/HomeScreenFragment.kt
+++ app/src/main/java/com/example/quickresponseapp/HomeScreenFragment.kt
@@ -1,15 +1,12 @@
 package com.example.quickresponseapp
 
 import android.content.Context
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.content.res.Configuration
 import android.content.res.Resources
 import android.preference.PreferenceManager
 import android.view.View
 import android.widget.ImageButton
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
